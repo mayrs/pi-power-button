405d209d0536f678390ca2bdd97df608f270b501
Nizhile <11645217+Nizhile@users.noreply.github.com>
2019-08-19 11:35:47 +0200

    Add long press to halt the system.

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -4,8 +4,19 @@
 from gpiozero import Button
 import subprocess
 
-button = Button(3)
+perform_halt = 0
+
+def longpress():
+    global perform_halt
+    perform_halt = 1
+
+button = Button(3, hold_time=2)
+button.when_held = longpress
 
 button.wait_for_press()
+button.wait_for_release()
 
-subprocess.call(['shutdown', '-h', 'now'], shell=False)
+if (perform_halt):
+    subprocess.call(['shutdown', '-h', 'now'], shell=False)
+else:
+    subprocess.call(['shutdown', '-r', 'now'], shell=False)
