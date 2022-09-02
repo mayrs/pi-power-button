9d839c2354296b9c9d4d28d98b163009ef6d01bd
Stefan Mayr <stefanmayr87@gmail.com>
2022-09-02 19:39:26 +0200

    Increase long press time

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -6,7 +6,7 @@ import subprocess
 def longpress():
     subprocess.call(['shutdown', '-h', 'now'], shell=False)
 
-button = Button(3, hold_time=2)
+button = Button(3, hold_time=3)
 button.when_held = longpress
 
 pause()
