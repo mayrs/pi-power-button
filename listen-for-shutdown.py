7ca10aaa886c876a9936f813fc7934c66028d357
Stefan Mayr <stefanmayr87@gmail.com>
2022-09-02 19:00:32 +0200

    Use long press to shut down the raspberry pi

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python
 
-
 from gpiozero import Button
 import subprocess
 
-perform_halt = 0
-
 def longpress():
-    global perform_halt
-    perform_halt = 1
+    subprocess.call(['shutdown', '-h', 'now'], shell=False)
 
 button = Button(3, hold_time=2)
 button.when_held = longpress
 
-button.wait_for_press()
-button.wait_for_release()
-
-if (perform_halt):
-    subprocess.call(['shutdown', '-h', 'now'], shell=False)
-else:
-    subprocess.call(['shutdown', '-r', 'now'], shell=False)
+pause()
